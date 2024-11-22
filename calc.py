9f1f17e913858528fa90c96752db5d7b07b28395
Andrei Hileuski <63409364+Uki-coder@users.noreply.github.com>
2024-11-22 13:44:14 +0100

    second and final commit all have been calculated

---
+++ calc.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import scipy as sci
+
+def voltage_time(x,a,b,c):
+    return c*np.exp(-x/b)+a
+
+def draw_calc(input_time, input_voltage, output_popt, output_err, p0):
+    mpl.use('TkAgg')
+    plt.rcParams['text.usetex'] = True
+
+    time = np.loadtxt(input_time)
+    voltage = np.loadtxt(input_voltage)
+    time_space = np.linspace(-0.02*max(time), 1.05*max(time), 1000)
+
+    fig, ax = plt.subplots()
+    ax.scatter(time, voltage)
+    ax.set_xlabel('$t$ [s]')
+    ax.set_ylabel('$U$ [V]')
+    plt.grid()
+
+    popt, pcov = sci.optimize.curve_fit(voltage_time, time, voltage, maxfev=10000)
+    perr = np.sqrt(np.diag(pcov))
+    np.savetxt(output_err, perr)
+    np.savetxt(output_popt, popt)
+
+    ax.plot(time_space, voltage_time(time_space, *popt), color='b', label='dopasowanie dla zależności $U(t)$')
+    ax.scatter(time, voltage, color='r', marker='o', label='pomiary $U(t)$')
+    ax.legend()
+    plt.show()
--- main.py
+++ main.py
@@ -1,53 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import scipy as sci
-#-------------------------------------------------------
-def voltage_time(x,a,b,c):
-    return c*np.exp(-x/b)+a
+from calc import draw_calc
 
-mpl.use('TkAgg')
-plt.rcParams['text.usetex'] = True
+draw_calc('data/time_2c.txt', 'data/voltage_2c.txt',\
+          'output/popt_2c.txt', 'output/error_2c.txt',\
+          [0,60,12])
 
-#2c--------------------------------------------------------
-time_2c = np.loadtxt('data/time_2c.txt')
-voltage_2c = np.loadtxt('data/voltage_2c.txt')
-time_space_2c = np.linspace(-2, max(time_2c) + 10, 1000)
+draw_calc('data/time_c.txt', 'data/voltage_c.txt',\
+          'output/popt_c.txt', 'output/error_c.txt',\
+          [0,30,12])
 
-fig, ax = plt.subplots()
-ax.scatter(time_2c, voltage_2c)
-ax.set_xlabel('$t$ [s]')
-ax.set_ylabel('$U$ [V]')
-plt.grid()
-
-popt, pcov = sci.optimize.curve_fit(voltage_time, time_2c, voltage_2c, maxfev=10000)
-perr = np.sqrt(np.diag(pcov))
-np.savetxt('output/error_2c.txt', perr)
-np.savetxt('output/popt_2c.txt', popt)
-
-ax.plot(time_space_2c, voltage_time(time_space_2c, *popt), color = 'b', label = 'dopasowanie dla zależności $U(t)$')
-ax.scatter(time_2c, voltage_2c, color='r', marker = 'o', label = 'pomiary $U(t)$')
-ax.legend()
-plt.show()
-
-#c----------------------------------------------------------
-
-time_c = np.loadtxt('data/time_c.txt')
-voltage_c = np.loadtxt('data/voltage_c.txt')
-time_space_c = np.linspace(-1, max(time_c) + 10, 1000)
-
-fig, ax = plt.subplots()
-ax.scatter(time_c, voltage_c)
-ax.set_xlabel('$t$ [s]')
-ax.set_ylabel('$U$ [V]')
-plt.grid()
-
-popt, pcov = sci.optimize.curve_fit(voltage_time, time_c, voltage_c)
-perr = np.sqrt(np.diag(pcov))
-np.savetxt('output/error_c.txt', perr)
-np.savetxt('output/popt_c.txt', popt)
-
-ax.plot(time_space_c, voltage_time(time_space_c, *popt), color = 'b', label = 'dopasowanie dla zależności $U(t)$')
-ax.scatter(time_c, voltage_c, color='r', label = 'pomiary $U(t)$')
-ax.legend()
-plt.show()
+draw_calc('data/time_c0.txt', 'data/voltage_c0.txt',\
+          'output/popt_c0.txt', 'output/error_c0.txt',\
+          [0,4.7,12])
